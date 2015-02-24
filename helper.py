660074d414332535209515ca6bc1e511930355e8
tallarin <sanfru.ivan@gmail.com>
2015-02-24 18:04:22 +0100

    Awful, but fully functional

---
+++ helper.py
@@ -0,0 +1,25 @@
+#! /usr/bin/env python
+
+import json
+import subprocess
+import base64   
+
+class Helper:
+
+    @staticmethod
+    def load_json(filename):
+
+        with open(filename, 'r') as f:
+            content = json.load(f)
+        f.close()
+
+        return content
+
+    @staticmethod
+    def run_dos_command():
+        pass
+
+    @staticmethod
+    def get_decode_string(string):
+        return base64.b64decode(string)
+
