7006edcfc153191140c9adecd8ce1a519e663219
ivanfoo <sanfru.ivan@gmail.com>
2015-06-17 00:50:58 +0200

    More work to rdos_helper

--- rdos.py
+++ rdos.py
@@ -12,74 +12,61 @@ from rdos_helper import RdosHelper as Helper
 class RemoteWindowsManager:
 
     def __init__(self, args):
+        self.data = {}
         self.args = args
-        self.datadir = os.path.dirname(__file__) + '/data/'
-        self.action = ''
-        self.target = ''
-
-        self.existing_actions = { "add": "add_cmd", "edit": "edit_cmd", "clear": "clear_cmd", "open": "open_cmd", "rm": "rm_cmd"}
+        self.datadir = os.path.dirname(__file__) + "/data/"
+        self.cmd = ""
+        self.target = ""
+        self.cmd_list = {
+            "add": "add_cmd",
+            "edit": "edit_cmd",
+            "clear": "clear_cmd",
+            "open": "open_cmd",
+            "rm": "rm_cmd"
+        }
 
     def get_target_data(self):
-        filename = self.datadir +  self.target + '.json'
+        filename = self.datadir +  self.target + ".json"
         return Helper.load_json(filename)
 
     def set_target_data(self, items):
-        filename = self.datadir +  self.target + '.json'
-        Helper.save_json(items, filename)
+        filename = self.datadir +  self.target + ".json"
+        Helper.dump_json(items, filename)
 
     def set_env(self):
-        if self.args[0] not in self.existing_actions:
+        if self.args[0] not in self.cmd_list:
             target = self.args[0]
-            self.args[0] = 'open'
+            self.args[0] = "open"
             self.args.append(target)
 
-        self.action = self.args[0]
+        self.cmd = self.args[0]
         self.target = self.args[1]
 
     def add_cmd (self):
-        items = {}
-        items['host'] = raw_input("host: ")
-        items['username'] = raw_input("username: ")
-        items['password'] = getpass.getpass()
+        target_data = {}
+        target_data["host"] = raw_input("host: ")
+        target_data["username"] = raw_input("username: ")
+        target_data["password"] = getpass.getpass()
         
-        self.set_target_data(items)
+        self.set_target_data(target_data)
 
     def open_cmd(self):
         self.data = self.get_target_data()
-        #self.set_cmdkeys()
-        #self.connect()
-        #self.clear()
+        Helper.set_cmdkeys()
+        Helper.connect()
 
     def rm_cmd(self):
         pass     
 
-    def set_cmdkeys(self):
-        host = "/generic:TERMSRV/" + self.data['host']
-        user = "/user:" + self.data['user']
-        passw = "/pass:" + base64.b64decode(self.data['passw'])
-
-        s.Popen(['cmdkey', host, user, passw], stdout=s.PIPE).wait()
-
-    def del_cmdkeys(self):
-        host = "/delete:TERMSRV/" + self.data['host']
-
-        s.Popen(['cmdkey', host], stdout=s.PIPE).wait()
-
-    def connect(self):
-        host = '/v:' + self.data['host'] + ':' + self.data['port']
-        rdp = self.datadir + self.data['rdp']
-
-        s.Popen(['mstsc', rdp, '/admin', host]).wait()
-
     def run(self):
         self.set_env()
-        getattr(RemoteWindowsManager, self.existing_actions[self.action])(self)
+        getattr(RemoteWindowsManager, self.cmd_list[self.cmd])(self)
         
-def main(target):
-    rdos = RemoteWindowsManager(target)
+def main(args):
+    rdos = RemoteWindowsManager(args)
     rdos.run()
 
-if __name__ == '__main__':
-    print '\n### Running rdos... ###\n'
+if __name__ == "__main__":
+    print "\n### Running rdos... ###\n"
     args = sys.argv[1:]
     main(args)
--- rdos_helper.py
+++ rdos_helper.py
@@ -10,9 +10,29 @@ class RdosHelper:
             return json.load(f)
 
     @staticmethod
-    def save_json(items, filename):
+    def dump_json(items, filename):
         with open(filename, 'w') as f:
-            json.dump(items, f)
+            json.dump(items, f, indent=4)
+
+    @staticmethod
+    def set_cmdkeys(host, user, passw):
+        host = "/generic:TERMSRV/" + host
+        user = "/user:" + user
+        passw = passw = "/pass:" + passw
+        s.Popen(["cmdkey", host, user, passw], stdout=s.PIPE).wait()
+
+    @staticmethod
+    def del_cmdkeys(self, host):
+        host = "/delete:TERMSRV/" + host
+        s.Popen(["cmdkey", host], stdout=s.PIPE).wait()
+
+    @staticmethod
+    def connect(self, host, rdp = 'base.rdp', port = 3389):
+        host = "/v:" + host + ":" + port
+        rdp = self.datadir + rdp
+        s.Popen(["mstsc", rdp, "/admin", host]).wait()
+
+
 
     
 
